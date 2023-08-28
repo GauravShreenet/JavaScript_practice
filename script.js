7cfd357dc856895afeec2e7db72a1d83d53f5e67
GauravShreenet <gthakuri656@gmail.com>
2023-08-28 11:01:55 +1000

    Template Literals

--- script.js
+++ script.js
@@ -1,99 +1,109 @@
 
-console.log('hello World!');
+// console.log('hello World!');
 
-// ============== variables
+// // ============== variables
 
-// var, let, const
+// // var, let, const
 
-var firstName; //creating variable name
-firstName = "Gaurav"
-let lastName = "Shreenet"
-const dob = "07-07-1997"
+// var firstName; //creating variable name
+// firstName = "Gaurav"
+// let lastName = "Shreenet"
+// const dob = "07-07-1997"
 
-console.log(firstName, lastName, dob)
- firstName = "Bidhya"
- lastName = "Shahi"
+// console.log(firstName, lastName, dob)
+//  firstName = "Bidhya"
+//  lastName = "Shahi"
 
-console.log(firstName, lastName, dob)
+// console.log(firstName, lastName, dob)
 
-// ============= js-operator
-// let a = 5;
+// // ============= js-operator
+// // let a = 5;
+// // console.log(a);
+// // const b = a;
+// // a = 10;
+// // console.log(a, b);
+
+
+
+
+
+// const a = {
+//     fistName: "Gaurav",
+//     lastName: "Shreenet",
+// };
 // console.log(a);
 // const b = a;
-// a = 10;
+// a.lastName = "smith";
+
 // console.log(a, b);
 
 
+// const pet = "cow"
+// // if (pet === "cat") {
+// //     console.log("meow")
+// // }
+// // else if (pet === "dog") {
+// //     console.log("bark")
+// // }
+// // else if (pet === "cow") {
+// //     console.log("Moo")
+// // }else{
+// //     console.log("no animal detected")
+// // }
 
+// switch (pet) {
+//     case "cat":
+//         console.log("meow");
+//         break;
 
+//         case "dog":
+//         console.log("bark");
+//         break;
 
-const a = {
-    fistName: "Gaurav",
-    lastName: "Shreenet",
-};
-console.log(a);
-const b = a;
-a.lastName = "smith";
+//         case "cow":
+//         console.log("moo");
+//         break;
 
-console.log(a, b);
-
-
-const pet = "cow"
-// if (pet === "cat") {
-//     console.log("meow")
-// }
-// else if (pet === "dog") {
-//     console.log("bark")
-// }
-// else if (pet === "cow") {
-//     console.log("Moo")
-// }else{
-//     console.log("no animal detected")
-// }
-
-switch (pet) {
-    case "cat":
-        console.log("meow");
-        break;
-
-        case "dog":
-        console.log("bark");
-        break;
-
-        case "cow":
-        console.log("moo");
-        break;
-
-        // case "":
-        // console.log("");
+//         // case "":
+//         // console.log("");
  
-        default:
-        console.log("no animal detected");
-        break;
-}
+//         default:
+//         console.log("no animal detected");
+//         break;
+// }
 
 
-try {
-    //code
-    console.log(first);
-}catch (error) {
-    //what you want to do when there is an error
-    console.log(error.message);
-}finally {
-   console.log("this code was executed at " + Date.now());
-}
+// try {
+//     //code
+//     console.log(first);
+// }catch (error) {
+//     //what you want to do when there is an error
+//     console.log(error.message);
+// }finally {
+//    console.log("this code was executed at " + Date.now());
+// }
 
-setTimeout (() => {
-    console.log("i am from settimeout")
-}, 3000);
+// setTimeout (() => {
+//     console.log("i am from settimeout")
+// }, 3000);
 
-let i = 0
-const counter = setInterval (() => {
-console.log("it is " + i++);
+// let i = 0
+// const counter = setInterval (() => {
+// console.log("it is " + i++);
 
-if (i === 10) {
-    clearInterval(counter);
-    console.log("it stopped");
-}
-}, 1000)
+// if (i === 10) {
+//     clearInterval(counter);
+//     console.log("it stopped");
+// }
+// }, 1000)
+
+
+// JS - ES6
+// Template literals
+
+const a = 5;
+const b = 6;
+const str = `when we add  ${a} and ${b} the total would be ${a+b}`;
+
+console.log(str);
 
