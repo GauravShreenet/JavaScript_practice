8de89c40d401d499c891416968b07f87d45caac1
GauravShreenet <gthakuri656@gmail.com>
2023-08-29 11:30:49 +1000

    practice array with loop

--- script.js
+++ script.js
@@ -169,9 +169,98 @@
 //     }
 // }
 
-const num = "045678976s";
-// const val = Number(num);
+// const num = "045678976s";
+// // const val = Number(num);
 
-const val = parseInt(num)
+// const val = parseInt(num)
 
-console.log(typeof val, val);
+// console.log(typeof val, val);
+
+// const vals = ["prem", 654, true, null, undefined, NaN, [234, 456], {}];
+
+
+const fruits = ["banana", "apple", "orange", "mango"];
+// fruits.push("mango"); //add at the last
+// fruits.shift("kiwi");
+// // fruits.
+// // fruits.pop(); //remove from the last
+// // fruits.upshift(); //remove from the begining
+
+// console.log(fruits);
+
+// fruits.forEach((item)=>{
+//     console.log(item, " is yummyt");
+// });
+
+// const newArg = fruits.map((item, i)=>{
+//     console.log(item, i);
+//     return item.toUpperCase();
+// });
+
+// console.log(newArg);
+
+// const newArg = fruits.filter((fruit)=>{
+// return !fruit.includes("e")
+// });
+// console.log(newArg);
+
+// const newArg = fruits.find((fruit) => fruit.includes("e"));
+// console.log(newArg)
+
+// const money = [3456, 2456, 23785, 334]
+// const total = money.reduce((subTtl, item) => {
+//     return subTtl + item;
+// }, 10)
+
+// console.log(total);
+
+// const arg = Array(100).fill("❤");
+// console.log(arg)
+
+// challenge #1
+
+// const array = Array(50).fill((Math.floor(Math.random()*100)));
+// console.log(array);
+
+// let randomArry = [];
+// let even = [];
+// let odd = [];
+
+// for (let i = 0; i < 50; i++){
+//     randomArry.push(Math.floor(Math.random()*100) + 1);
+// }
+
+// console.log(randomArry);
+// console.log(randomArry.sort((a, b) => b - a));
+
+// const total = randomArry.reduce((subTtl, item)=> {
+//     return subTtl + item;
+// }, 0);
+
+// console.log(total);
+
+// for (const num of randomArry) {
+//     if (num % 2 === 0){
+//         even.push(num);
+//     }else{
+//         odd.push(num);
+//     }
+// }
+
+// console.log(even);
+// console.log(odd);
+
+// const noDuplicate = randomArry.filter((item, index) => randomArry.indexOf(item) === index);
+// console.log(noDuplicate);
+
+// // 1,3,4,11,6,5,2
+
+let uniquesRandom = [];
+for (let i = 0; i < 50; i++) {
+    const n = Math.floor(Math.random()*100) + 1;
+    if (!uniquesRandom.includes(n)){
+        uniquesRandom.push(n);
+    }
+    
+}
+console.log(uniquesRandom.sort());
